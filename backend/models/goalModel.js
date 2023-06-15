270cb82c137d7c90ef7cd02152d3738371da6ea2
Kash <kash.singh1989@gmail.com>
2023-06-16 00:48:53 +0100

    updated goal model to have reference of user model

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -3,11 +3,15 @@ import mongoose from "mongoose";
 //Define the goal schema
 const goalSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     text: {
       type: String,
       required: true,
       unique: true,
-      trime: true,
     },
   },
   { timestamps: true }
